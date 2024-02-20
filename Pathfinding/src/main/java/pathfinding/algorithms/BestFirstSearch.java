321de715dfefe50e8e95a76ee06a506eded0b0bc
Spi3lot <spi3lot@gmail.com>
2024-02-20 20:56:47 +0100

    Add 2 new overloaded versions to PathfindingAlgorithm to support conditional exit points

--- Pathfinding/src/main/java/pathfinding/algorithms/BestFirstSearch.java
+++ Pathfinding/src/main/java/pathfinding/algorithms/BestFirstSearch.java
@@ -4,6 +4,7 @@ import pathfinding.graphs.Graph;
 import pathfinding.service.PathTracer;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 /**
  * Abstract class to make the implementation of best-first search algorithms easier.
@@ -13,51 +14,98 @@ import java.util.*;
 public abstract class BestFirstSearch<T> implements PathfindingAlgorithm<T> {
 
     protected final Map<T, Double> distances = new HashMap<>();
+    private final Map<T, T> predecessors = new HashMap<>();
+    private final PathTracer<T> pathTracer = new PathTracer<>(predecessors);
 
     @Override
     public Optional<List<T>> findShortestPath(T start,
                                               T end,
                                               Graph<T> graph) {
-        var queue = new PriorityQueue<>(
-                Comparator.<T>comparingDouble(current -> f(current, end))
-                        .thenComparingDouble(current -> h(current, end))
-        );
-
-        var predecessors = new HashMap<T, T>();
-        distances.clear();
-        distances.put(start, 0.0);
-        queue.add(start);
+        var queue = new PriorityQueue<>(createComparator(end));
+        setupDataStructures(start, queue);
 
         while (!queue.isEmpty()) {
             var current = queue.poll();
 
             if (current == end) {
-                var pathTracer = new PathTracer<>(predecessors);
                 return Optional.of(pathTracer.unsafeTrace(start, end));
             }
 
             graph.getNeighbors(current)
-                    .forEach((neighbor, weight) -> {
-                        double newDistance = weight + f(current, end);
-
-                        if (newDistance < f(neighbor, end)) {
-                            distances.put(neighbor, newDistance);
-                            predecessors.put(neighbor, current);
-                            queue.remove(neighbor);
-                            queue.offer(neighbor);
-                        }
-                    });
+                    .forEach((neighbor, weight) -> updateIfBetter(
+                            neighbor,
+                            current,
+                            queue,
+                            f(current, end) + weight,
+                            f(neighbor, end)
+                    ));
         }
 
         return Optional.empty();
     }
 
-    protected double f(T current, T end) {
-        return g(current) + h(current, end);
+    @Override
+    public Optional<List<T>> findShortestPath(T start,
+                                              Predicate<T> endCondition,
+                                              Graph<T> graph) {
+        var queue = new PriorityQueue<>(createComparator());
+        setupDataStructures(start, queue);
+
+        while (!queue.isEmpty()) {
+            var current = queue.poll();
+
+            if (endCondition.test(current)) {
+                return Optional.of(pathTracer.unsafeTrace(start, current));
+            }
+
+            graph.getNeighbors(current)
+                    .forEach((neighbor, weight) -> updateIfBetter(
+                            neighbor,
+                            current,
+                            queue,
+                            g(current) + weight,
+                            g(neighbor)
+                    ));
+        }
+
+        return Optional.empty();
     }
 
-    protected abstract double g(T current);
+    private Comparator<T> createComparator(T end) {
+        return Comparator.<T>comparingDouble(vertex -> f(vertex, end))
+                .thenComparingDouble(vertex -> h(vertex, end));
+    }
 
-    protected abstract double h(T current, T end);
+    private Comparator<T> createComparator() {
+        return Comparator.comparingDouble(this::g);
+    }
+
+    private void setupDataStructures(T start, Queue<T> queue) {
+        predecessors.clear();
+        distances.clear();
+        distances.put(start, 0.0);
+        queue.add(start);
+    }
+
+    private void updateIfBetter(T neighbor,
+                                T current,
+                                Queue<T> queue,
+                                double newDistance,
+                                double distance) {
+        if (newDistance < distance) {
+            distances.put(neighbor, newDistance);
+            predecessors.put(neighbor, current);
+            queue.remove(neighbor);
+            queue.offer(neighbor);
+        }
+    }
+
+    protected double f(T vertex, T end) {
+        return g(vertex) + h(vertex, end);
+    }
+
+    protected abstract double g(T vertex);
+
+    protected abstract double h(T vertex, T end);
 
 }
--- Pathfinding/src/main/java/pathfinding/algorithms/PathfindingAlgorithm.java
+++ Pathfinding/src/main/java/pathfinding/algorithms/PathfindingAlgorithm.java
@@ -3,22 +3,37 @@ package pathfinding.algorithms;
 import pathfinding.graphs.Graph;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Predicate;
 
-public interface PathfindingAlgorithm<T>{
+public interface PathfindingAlgorithm<T> {
+
+    /**
+     * Searches any path between two given vertices in a graph.
+     * @param start the vertex the path starts at
+     * @param end the vertex the path ends at
+     * @param graph the graph in which a path is to be found
+     * @return Any path between the two vertices, or {@link Optional#empty()} if no path exists.
+     */
+    default Optional<List<T>> findAnyPath(T start,
+                                          T end,
+                                          Graph<T> graph) {
+        return findAnyPath(start, vertex -> vertex.equals(end), graph);
+    }
 
     /**
      * Searches any path between two given vertices in a graph.
      *
      * @param start the vertex the path starts at
-     * @param end the vertex the path ends at
+     * @param endCondition the condition that has to be met for the path to end
      * @param graph the graph in which a path is to be found
-     * @return Any path between the two vertices, or <code>Optional.empty</code> if no path exists.
+     * @return Any path between the two vertices, or {@link Optional#empty()} if no path exists.
      */
     default Optional<List<T>> findAnyPath(T start,
-                                          T end,
+                                          Predicate<T> endCondition,
                                           Graph<T> graph) {
-        return findShortestPath(start, end, graph);
+        return findShortestPath(start, endCondition, graph);
     }
 
     /**
@@ -27,10 +42,23 @@ public interface PathfindingAlgorithm<T>{
      * @param start the vertex the path starts at
      * @param end   the vertex the path ends at
      * @param graph the graph in which the shortest path is to be found
-     * @return The shortest path between the two vertices, or <code>Optional.empty</code> if no path exists.
+     * @return The shortest path between the two vertices, or {@link Optional#empty()} if no path exists.
+     */
+    default Optional<List<T>> findShortestPath(T start,
+                                               T end,
+                                               Graph<T> graph) {
+        return findShortestPath(start, vertex -> vertex.equals(end), graph);
+    }
+
+    /**
+     * Searches the shortest path between two given vertices in a graph.
+     *
+     * @param start        the vertex the path starts at
+     * @param endCondition the condition that has to be met for the path to end
+     * @return The shortest path between the two vertices, or {@link Optional#empty()} if no path exists.
      */
     Optional<List<T>> findShortestPath(T start,
-                                       T end,
+                                       Predicate<T> endCondition,
                                        Graph<T> graph);
 
 
