5fb2c42a2d3a4cf1ed9912c14cd79d6862bb03b1
Spi3lot <spi3lot@gmail.com>
2024-03-07 19:03:38 +0100

    modifiable benchmark

--- Pathfinding/src/main/java/pathfinding/graphs/CachedGraph.java
+++ Pathfinding/src/main/java/pathfinding/graphs/CachedGraph.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * An abstract class that helps with caching the neighbors of a vertex in a graph.
+ * An abstract class that helps with memoizing the neighbors of a graph.
  * The neighbors will only be calculated when they are requested for the first time.
  *
  * @param <T> the type of the vertices in the graph
